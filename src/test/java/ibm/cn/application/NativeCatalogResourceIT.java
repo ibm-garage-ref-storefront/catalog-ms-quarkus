76c4da5416a0b5e48b0c104175ae0c28b04d07ba
user1 <user1@example.com>
2021-02-08 13:25:57 +0530

    first commit quarkus template

---
+++ src/test/java/ibm/cn/application/NativeCatalogResourceIT.java
@@ -0,0 +1,9 @@
+package ibm.cn.application;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCatalogResourceIT extends CatalogResourceTest {
+
+    // Execute the same tests but in native mode.
+}
