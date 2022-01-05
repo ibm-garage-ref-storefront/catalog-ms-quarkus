5d39e88dbc0b1d0c8e7f1a9b9bdc0a930eedc452
Hemankita Perabathini <phemankita@gmail.com>
2022-01-05 19:00:49 +0530

    adding fault tolerance
    Signed-off-by: Hemankita Perabathini <phemankita@gmail.com>

--- src/main/java/ibm/cn/application/CatalogResource.java
+++ src/main/java/ibm/cn/application/CatalogResource.java
@@ -1,6 +1,7 @@
 package ibm.cn.application;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -10,6 +11,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 
 import ibm.cn.application.model.Item;
 import ibm.cn.application.repository.ItemService;
@@ -22,10 +26,20 @@ public class CatalogResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Retry(maxRetries = 2, maxDuration = 2000)
+    @Fallback(fallbackMethod = "fallbackInventory")
     public List<Item> getInventory() throws IOException {
     	List<Item> items = null;
         items = itemsRepo.findAll();
         return items;
+    }
+    
+    public List<Item> fallbackInventory() {
+        //Returns a default fallback list
+        List<Item> list = new ArrayList<Item>();
+        Item item = new Item(0, "Standard fallback message", "Standard fallback message", 0, "Standard fallback message", "Catalog-fallback.jpg", 0);
+        list.add(item);
+        return list;
     }
     
     @GET
